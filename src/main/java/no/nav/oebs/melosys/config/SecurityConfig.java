551664bfa41f1506c80d36e1ca60fc7b8ec50f2f
rasmusvknav <113107378+rasmusvknav@users.noreply.github.com>
2023-01-13 13:45:51 +0100

    La til Conditional annotasjon for aktiv spring profil

--- src/main/java/no/nav/oebs/melosys/config/SecurityConfig.java
+++ src/main/java/no/nav/oebs/melosys/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package no.nav.oebs.melosys.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
@@ -8,7 +9,8 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
  * Konfigurasjonsklasse for token-basert sikkerhet.
  */
 @Configuration
-//@EnableJwtTokenValidation(ignore = { "org.springframework", "org.springdoc" })
+@EnableJwtTokenValidation(ignore = { "org.springframework", "org.springdoc" })
+@ConditionalOnProperty(prefix = "spring.profiles", name = "active", havingValue = "nais")
 public class SecurityConfig {
 
 }
