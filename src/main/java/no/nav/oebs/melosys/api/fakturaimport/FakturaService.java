e9b17042b0d058d1604764e2272bf142ec7451a5
rasmusvknav <113107378+rasmusvknav@users.noreply.github.com>
2023-01-13 13:45:14 +0100

    Slettet gammel kode for validerkontostreng api

--- src/main/java/no/nav/oebs/melosys/api/fakturaimport/FakturaService.java
+++ src/main/java/no/nav/oebs/melosys/api/fakturaimport/FakturaService.java
@@ -26,84 +26,8 @@ public class FakturaService extends ObjektMaps {
 
 	public FakturaService(PlsqlProcedureRepository plsqlProcedureRepository, ObjectMapper objectMapper) {
 		super(objectMapper);
+		log.info("Oppretter Faktura service...");
 		this.plsqlProcedureRepository = plsqlProcedureRepository;
-	}
-
-	public String finnValiderKontoStreng(Integer org_id,
-			   String artskonto,
-			   String ksted,
-			   String produktoppgave,
-			   String deloppgave,
-			   String fellesoppgave,
-			   String statskonto,
-			   String kilde,
-			   String tilsagnsaar,
-			   String fritt_felt_1,
-			   String fritt_felt_2,
-			   String fullmaktskode,
-			   String regnskapsforer,
-			   String system) {
-
-
-	//public List<Kryssvalideringtransaksjon> finnKryssvalideringtransaksjoner(String companycode, String segmentname,
-	//															LocalDate lastupdatedate) {
-
-		PlsqlProcedureResult result = executePlsqlProcedure(buildRequest(org_id,
-				artskonto,
-				ksted,
-				produktoppgave,
-				deloppgave,
-				fellesoppgave,
-				statskonto,
-				kilde,
-				tilsagnsaar,
-				fritt_felt_1,
-				fritt_felt_2,
-				fullmaktskode,
-				regnskapsforer,
-				system));
-		/* if (result.getMessageNumber() < 0) {
-			 throwPlsqlException(result);
-		/} */
-
-		return result.getData();
-
-		// return getApiResponse(result.getData());
-	}
-
-	/**
-	 * Bygger et requestobjekt som skal konverteres til JSON.
-	 */
-	private FakturaRequest buildRequest(Integer org_id,
-										String artskonto,
-										String ksted,
-										String produktoppgave,
-										String deloppgave,
-										String fellesoppgave,
-										String statskonto,
-										String kilde,
-										String tilsagnsaar,
-										String fritt_felt_1,
-										String fritt_felt_2,
-										String fullmaktskode,
-										String regnskapsforer,
-										String system) {
-		return FakturaRequest.builder()
-				.org_id(org_id)
-				.artskonto(artskonto)
-				.ksted(ksted)
-				.produktoppgave(produktoppgave)
-				.deloppgave(deloppgave)
-			  	.fellesoppgave(fellesoppgave)
-			 	.statskonto(statskonto)
-				.kilde(kilde)
-				.tilsagnsaar(tilsagnsaar)
-				.fritt_felt_1(fritt_felt_1)
-				.fritt_felt_2(fritt_felt_2)
-				.fullmaktskode(fullmaktskode)
-				.regnskapsforer(regnskapsforer)
-				.system(system)
-				.build();
 	}
 
 	/**
