ac719486e5d8153539691363fc581f65cb08eac1
rasmusvknav <113107378+rasmusvknav@users.noreply.github.com>
2023-08-24 09:25:19 +0200

    Opdatert med endringer for å matche melding fra OeBS, og ny custom deserializer for LocalDate

--- src/main/java/no/nav/oebs/melosys/db/entity/FakturaStatusFeilImport.java
+++ src/main/java/no/nav/oebs/melosys/db/entity/FakturaStatusFeilImport.java
@@ -1,26 +1,25 @@
 package no.nav.oebs.melosys.db.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class FakturaStatusFeilImport extends FakturaStatus {
-    String feilmelding;
+
 
     public FakturaStatusFeilImport(String fakturaReferanseNr, String feilmelding) {
         super(fakturaReferanseNr,
                 "",
-                LocalDateTime.now(),
+                LocalDate.now(),
                 "FEIL",
                 BigDecimal.valueOf(0),
-                BigDecimal.valueOf(0));
-        this.feilmelding = feilmelding;
+                BigDecimal.valueOf(0),
+                feilmelding);
+
     }
 
 
