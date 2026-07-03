e99e5c80f8386bee981338916d70cdee6a3a9437
Agnes Oftung Strandbu <agnes.oftung.strandbu@nav.no>
2026-07-03 09:33:22 +0200

    fix(OEBS-2191): move jackson property from application.yaml to JacksonConfig

--- src/main/java/no/nav/oebs/melosys/config/JacksonConfig.java
+++ src/main/java/no/nav/oebs/melosys/config/JacksonConfig.java
@@ -5,6 +5,7 @@ import java.util.TimeZone;
 import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tools.jackson.core.json.JsonReadFeature;
 import tools.jackson.databind.DeserializationFeature;
 
 
@@ -22,6 +23,7 @@ public class JacksonConfig {
 	public JsonMapperBuilderCustomizer jacksonCustomizer() {
 		return jsonMapperBuilder -> jsonMapperBuilder
 				.disable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+				.enable(JsonReadFeature.ALLOW_LEADING_ZEROS_FOR_NUMBERS)
 				.defaultTimeZone(TimeZone.getDefault());
 	}
 }
